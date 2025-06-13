df1a62d3683a06bff2403e8924d03d1056983d01
bicity2 <bicity2@gmail.com>
2025-06-13 14:29:12 +0900

    printAdd()メソッドの追加

--- src/Calc.java
+++ src/Calc.java
@@ -5,6 +5,14 @@ public class Calc {
 		// TODO 自動生成されたメソッド・スタブ
 		
 		// ★ ここから下を実装 ★
+		printAdd(2,3);
+
+	}
+	
+	public static void printAdd(int arg1, int arg2) {
+		int sum = arg1 + arg2;
+		String result = arg1 + "+" + arg2 + "=" + sum;
+		System.out.println(result);
 	}
 
 }
