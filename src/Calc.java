c212b27dc717593bc27329c04702593e764aa4bb
bicity2 <bicity2@gmail.com>
2025-06-13 14:30:03 +0900

    printSub()メソッドの追加

--- src/Calc.java
+++ src/Calc.java
@@ -3,15 +3,20 @@ public class Calc {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
-		// ★ ここから下を実装 ★
 		printAdd(2,3);
-
+		// ★ ここから下を実装 ★
+		printSub(7,4);
 	}
 	
 	public static void printAdd(int arg1, int arg2) {
 		int sum = arg1 + arg2;
 		String result = arg1 + "+" + arg2 + "=" + sum;
+		System.out.println(result);
+	}
+
+	public static void printSub(int arg1, int arg2) {
+		int difference  = arg1 - arg2;
+		String result = arg1 + "-" + arg2 + "=" + difference ;
 		System.out.println(result);
 	}
 
